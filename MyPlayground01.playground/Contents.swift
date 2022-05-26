d91591447934dcf0a3dca45a8b5d347c6aa57a3b
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-05-26 15:13:33 +0800

    Add playground file

---
+++ MyPlayground01.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Combine
+import SwiftUI
+import PlaygroundSupport
+
+struct TestView: View {
+  @State var input: String = ""
+  var body: some View {
+    Text("Hello, playground")
+    TextField("Clear input field...", text: $input)
+      .textFieldStyle(ClearTextFieldStyle())
+  }
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: TestView())
+
+// Custom TextFieldStyle
+struct ClearTextFieldStyle: TextFieldStyle {
+  func _body(configuration: TextField<_Label>) -> some View {
+    HStack {
+      configuration
+
+      let mirror = Mirror(reflecting: configuration)
+      let textBinding = mirror.descendant("_text") as! Binding<String>
+
+      Button {
+        textBinding.wrappedValue = ""
+      } label: {
+        Image(systemName: "clear")
+      }
+      .padding(10)
+      .border(.gray, width: 1)
+      .disabled(textBinding.wrappedValue.isEmpty)
+    }
+    .padding(10)
+    .border(.green, width: 1)
+  }
+}
